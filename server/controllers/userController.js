15574de0cb8bf1dfe92a633b62dae158b208afd4
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2025-01-29 17:59:36 +0200

    Added route for booking visit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,3 +18,76 @@ if(!userExist){
 }
 
 })
+
+export const bookVisit = asyncHandler(async (req, res) => {
+  const {id} = req.params;
+  const {email, date} = req.body;
+
+  try{
+      const booked = await prisma.user.findUnique({
+        where: {email},
+        select: {bookedVisit: true}
+      })
+
+      if(booked.bookedVisit.some((visit) => visit.id === id)){
+        res.status(400).json({message: "This residency is already booked by you!"})
+      }else{
+        await prisma.user.update({
+          where:{email:email},
+          data: {
+            bookedVisit: {push: {id, date}}
+          }
+        })
+      }
+      res.send("Your visit is booked successfully!")
+  }catch(err){
+    throw new Error(err.message);
+  }
+})
+
+export const getAllBookings = asyncHandler(async(req, res) => {
+  const {email} = req.body;
+
+  try{
+    const allBookings = await prisma.user.findUnique({
+      where: {email},
+      select: {bookedVisit: true}
+    })
+    res.status(200).send(allBookings)
+  }catch(err){
+    throw new Error(err.message)
+  }
+ 
+})
+
+export const cancelBooking = asyncHandler(async (req, res) => {
+  const {email} = req.body;
+  const {id} = req.params;
+
+  try {
+
+    const user = await prisma.user.findUnique({
+      where: {email: email},
+      select: {bookedVisit: true}
+    })
+
+    const index = user.bookedVisit.findIndex((visit) => visit.id === id);
+
+    if(index === -1){
+      res.status(404).json({message: "Booking not found"});
+    }else{
+      user.bookedVisit.splice(index,1);
+
+      await prisma.user.update({
+        where: {email},
+        data: {
+          bookedVisit: user.bookedVisit
+        }
+      })
+
+      res.send('Booking cancelled successfully!')
+    }
+  }catch(err){
+    throw new Error(err.message);
+  }
+})
