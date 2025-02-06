c0d6f3813d1d6e64c5b55e37afb6b3a91b797e2d
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2025-02-06 15:32:21 +0200

    Updated utils

---
+++ client/src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Най-добри лихвени проценти на пазара",
+    detail:
+      "Голяма възможност да построите мечтания си дом...",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Предотвратяване на нестабилни цени",
+    detail:
+      "Голяма възможност да построите мечтания си дом...",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Най-добра цена на пазара",
+    detail:
+      "Голяма възможност да построите мечтания си дом...",
+  },
+];
+export default data;
