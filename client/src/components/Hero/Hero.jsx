32e491e6104450aba8aaef15780ca772009919b4
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2025-02-13 18:43:18 +0200

    Added more style in hero compoents

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -6,14 +6,15 @@ const Hero = () => {
             <div className="hero-container">
 
                 <div className="hero-left">
-                    Left Section
+                    <h1>Msell Reall Estate</h1>
+                    <p>На една стъпка от мечтания дом!</p>
+                      
                 </div>
 
                 <div className="hero-right">
                     <div className="img-container">
                         <img src="./hero-profile.jpg" alt="" />
                     </div>
-                    Right Section
                 </div>
             </div>
         </section>
