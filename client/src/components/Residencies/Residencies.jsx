b61313d500e0ec43eeba420ea058b5d336fdb7cf
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2025-02-18 12:52:53 +0200

    Added slider

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -2,7 +2,7 @@ import 'react'
 import {Swiper, SwiperSlide, useSwiper} from 'swiper/react';
 import 'swiper/css';
 import './Residencies.css';
-import '../../utils/slider.json'
+import data from '../../utils/slider.json'
 
 function Residencies() {
   return (
@@ -11,6 +11,26 @@ function Residencies() {
         <div className="r-head ">
           <span>Най-популярни предложения</span>
         </div>
+
+        <Swiper>
+          {
+            data.map((res, i) => (
+             <SwiperSlide key={i}>
+              <div className="r-card">
+              <img src={res.image} style={{width: "30%", height: "30%"}} alt="home" />
+
+              <span className="r-price">
+              <span>{res.price} </span>
+              <span style={{color: " #ff4400"}}>€ </span>
+              </span>
+
+              <span className='resName'>{res.name}</span>
+              <span className='resName'>{res.detail}</span>
+              </div>
+             </SwiperSlide>
+            ))
+          }
+        </Swiper>
       </div>
     </section>
   )
